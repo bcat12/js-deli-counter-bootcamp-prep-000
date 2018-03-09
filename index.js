a21ba07531eb4bef41c5bcd8c509a6fedcccaf5b
Brian Caterfino <bcaterfino@gmail.com>
2018-03-09 18:56:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,10 +4,14 @@ function takeANumber(katzDeliLine, name){
  return  `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
 }
 
-function nowServing(deliLine){
-  
-  if (deliLine.length===0)
-   return "There is nobody waiting to be served!"
-  var name = delLine.shift();
- return "Currently serving "+ name + "."
+function nowServing(katzDeli) {
+  let i = 0;
+  while (i < katzDeli.length) {
+    i++;
+  }
+  if (katzDeli.length === 0) {
+    return "There is nobody waiting to be served!";
+  }
+  else
+  return (`Currently serving ${katzDeli.shift()}.`);
 }
