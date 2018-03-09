94534d5f952230fe5a3a8fce2016a23891995c65
Brian Caterfino <bcaterfino@gmail.com>
2018-03-09 18:40:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ function takeANumber(katzDeliLine, name){
 }
 
 function nowServing(){
-  return katzDeliLine[0]
-  delete katzDeliLine[0]
   if (katzDeliLine.length===0)
    return "There is nobody waiting to be served!"
+  else return katzDeliLine[0]
+  delete katzDeliLine[0]
+  
 }
