5a655050f0f286ed5a3d5099157e27bbc221c063
Brian Caterfino <bcaterfino@gmail.com>
2018-03-09 18:43:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ function nowServing(){
   if (katzDeliLine.length===0)
    return "There is nobody waiting to be served!"
   else return 'Currently serving ${katzDeliLine[0]}'
-  delete katzDeliLine[0]
+    delete katzDeliLine[0]
   
 }
