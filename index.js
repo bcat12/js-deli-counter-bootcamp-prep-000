e8e30dbd343924c0dbe4d99abf02e0ca97cfee90
Brian Caterfino <bcaterfino@gmail.com>
2018-03-09 19:16:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,19 +16,12 @@ function nowServing(katzDeliLine) {
   return (`Currently serving ${katzDeliLine.shift()}.`);
 }
 
-function currentLine(katzDeliLine){
-  
-var i=0;
-   if (katzDeliLine.length === 0) {
-  return "The line is currently empty.";
-  
-  
-  while( i<katzDeliLine.length){
-    i++;
-  }
-  var newarray= []
-  }
-  else
-  newarray += newarray.push(` ${i+1}. ${katzDeliLine[i]}`);
-  return "The line is currently:"+ newarray
-}
+function currentLine(line) {
+   var lineNum = [];
+   if (line.length == 0) {
+     return "The line is currently empty.";
+   }
+   for (var i = 0; i < line.length; i++) {
+     lineNum.push(` ${i+1}. ${line[i]}`);
+ 	}
+   return "The line is currently:" + lineNum;
