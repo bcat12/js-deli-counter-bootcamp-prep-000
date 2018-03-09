f9630f6c312f2d9628a23ed862bd5040f6f3db74
Brian Caterfino <bcaterfino@gmail.com>
2018-03-09 19:15:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,10 +17,12 @@ function nowServing(katzDeliLine) {
 }
 
 function currentLine(katzDeliLine){
+  
+var i=0;
    if (katzDeliLine.length === 0) {
   return "The line is currently empty.";
   
-  var i=0;
+  
   while( i<katzDeliLine.length){
     i++;
   }
