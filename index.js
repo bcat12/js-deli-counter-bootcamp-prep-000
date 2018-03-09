9a3351ce1f383141ccfe321ba1a4994af898d604
Brian Caterfino <bcaterfino@gmail.com>
2018-03-09 19:12:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,6 +26,6 @@ function currentLine(katzDeliLine){
   return "The line is currently empty.";
   }
   else
-  new array += array.push(i +"." + katzDeliLine[i]);
+  newarray += array.push(i +"." + katzDeliLine[i]);
   return "The line is currently:"+ newarray
 }
