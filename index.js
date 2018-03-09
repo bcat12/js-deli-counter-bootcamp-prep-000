a9d9a15adb448e011a0a8a70a136fcbab8e94be5
Brian Caterfino <bcaterfino@gmail.com>
2018-03-09 18:31:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var katzDeli= [];
 function takeANumber(katzDeli, name){
   katzDeli.push('${name}');
-  return ('Welcome, ${name}. You are number ${katzDeli} in line.')
+  return ('Welcome, ${name}. You are number ${katzDeli.length} in line.')
 }
