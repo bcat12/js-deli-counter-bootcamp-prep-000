01aa94b3bc3712772429adb117fe148034fccf57
Brian Caterfino <bcaterfino@gmail.com>
2018-03-09 18:37:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ function takeANumber(katzDeliLine, name){
   katzDeliLine.push(name);
  return  `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
 }
+
+function nowServing(){
+  if (katzDeliLine==0)
+   return "the line is empty"
+}
